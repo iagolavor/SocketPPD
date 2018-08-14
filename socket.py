095636cac30cc46a9760202051f725e69d93b3a6
Iago <iago-l@hotmail.com>
2018-08-14 20:54:11 -0300

    Novo codigo de socket, não importa muito, depois vou fazer denovo

--- socket.py
+++ socket.py
@@ -1,14 +1,33 @@
-#Criar um INET, STREAMing socket
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#bind the socket to a public host, and a well-known port
-serversocket.bind((socket.gethostname(), 80))
-#virar um socket servidor
-serversocket.listen(5)
+class MySocket:
+    """demonstration class only
+      - coded for clarity, not efficiency
+    """
 
-while True:
-    #aceitar conexões de fora
-    (clientsocket, address) = serversocket.accept()
-    #fazer algo com o socket cliente
-    #nesse caso vamos fingir que é um servidor com threads
-    ct = client_thread(clientsocket)
-    ct.run()
+    def __init__(self, sock=None):
+        if sock is None:
+            self.sock = socket.socket(
+                            socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+
+    def connect(self, host, port):
+        self.sock.connect((host, port))
+
+    def mysend(self, msg):
+        totalsent = 0
+        while totalsent < MSGLEN:
+            sent = self.sock.send(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+
+    def myreceive(self):
+        chunks = []
+        bytes_recd = 0
+        while bytes_recd < MSGLEN:
+            chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
+            if chunk == b'':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+        return b''.join(chunks)
